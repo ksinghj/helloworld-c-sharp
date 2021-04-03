96cc34a9b0904af00c96df0ea3fbc58700abadae
Kartar <kartarsjabanda@gmail.com>
2021-04-03 13:53:24 +0100

    first commit to GitHub

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public string FirstName;
+
+        public string LastName;
+
+        public void SayName()
+        {
+            Console.WriteLine($"My name is {FirstName} {LastName}");
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Kartar = new Person();
+
+            Kartar.FirstName = "Kartar";
+            Kartar.LastName = "Singh";
+
+            Kartar.SayName();
+        }
+    }
+}
